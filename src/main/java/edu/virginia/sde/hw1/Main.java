3f071ec757f512128fd698624fda2a651424767a
Eric <fsm6wv@portal.cs.virginia.edu>
2023-09-02 18:54:53 -0400

    implementing checks for user arg # of reps

--- src/main/java/edu/virginia/sde/hw1/Main.java
+++ src/main/java/edu/virginia/sde/hw1/Main.java
@@ -2,27 +2,33 @@ package edu.virginia.sde.hw1;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
+        int Representatives = 435;
+        //Resource Used: https://docs.oracle.com/javase%2F7%2Fdocs%2Fapi%2F%2F/java/lang/Integer.html
+        //Description: used Integer class to parse/check user input
         try{
             int numReps = Integer.parseInt(args[1]);
             if(numReps<=0) {
                 System.out.println("Invalid User Input - Number of representatives must be positive and nonzero");
                 System.exit(0);
             }
+            Representatives = numReps;
         }
         catch(NumberFormatException e){
             System.out.println("Invalid User Input - Number of representatives must be an integer");
             System.exit(0);
         }
-
-
-        System.out.println("Hello World!");
-        String file = args[0];
-        try {
-            FileReader filereader = new FileReader(file);
+        catch(IndexOutOfBoundsException n){
+            //do nothing
+            //no 2nd arg, default 435
         }
-        catch(Exception e){
+        System.out.println(Representatives);
+        //String file = args[0];
+        //try {
+        //    FileReader filereader = new FileReader(file);
+        //}
+        //catch(Exception e){
 
-        }
+        //}
 
     }
 }
