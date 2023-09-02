f3fc8dc1a8e1815f2329206a4685c845f97f19b9
Eric <fsm6wv@portal.cs.virginia.edu>
2023-09-02 18:24:44 -0400

    implementing checks for user arg # of reps

--- src/main/java/edu/virginia/sde/hw1/Main.java
+++ src/main/java/edu/virginia/sde/hw1/Main.java
@@ -1,12 +1,28 @@
 package edu.virginia.sde.hw1;
-
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
+        try{
+            int numReps = Integer.parseInt(args[1]);
+            if(numReps<=0) {
+                System.out.println("Invalid User Input - Number of representatives must be positive and nonzero");
+                System.exit(0);
+            }
+            }
+        catch(NumberFormatException e){
+            System.out.println("Invalid User Input - Number of representatives must be an integer");
+            System.exit(0);
+        }
+
+
         System.out.println("Hello World!");
+        String file = args[0];
+        try {
+            FileReader filereader = new FileReader(file);
+        }
+        catch(Exception e){
+
+        }
+
     }
-
-
-
-
-
 }
