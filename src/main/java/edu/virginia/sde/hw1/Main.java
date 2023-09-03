49f2e7b41bce20953b446717755197c0bb466104
Eric <fsm6wv@portal.cs.virginia.edu>
2023-09-02 22:42:25 -0400

    csvreader import working

--- src/main/java/edu/virginia/sde/hw1/Main.java
+++ src/main/java/edu/virginia/sde/hw1/Main.java
@@ -1,12 +1,12 @@
 package edu.virginia.sde.hw1;
 import java.io.*;
 import java.lang.*;
-//import com.opencsv.CSVReader;
+import com.opencsv.CSVReader;
 public class Main {
     public static void main(String[] args) {
         int Representatives = 435;
         //Resource Used: https://docs.oracle.com/javase%2F7%2Fdocs%2Fapi%2F%2F/java/lang/Integer.html
-        //Description: used Integer class to parse/check user input
+        //Description: used Integer class to parse/check user input (line 11)
         try{
             int numReps = Integer.parseInt(args[1]);
             if(numReps<=0) {
@@ -25,12 +25,14 @@ public class Main {
         }
         System.out.println(Representatives);
         String file = args[0];
+        //Resource Used: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+        //Description: learning about try-with-resources (line 30)
         try(FileReader filereader = new FileReader(file); CSVReader csvReader = new CSVReader(filereader)) {
 
-        }
-        catch(Exception e){
+        //}
+        //catch(Exception e){
 
-        }
+        //}
 
     }
 }
