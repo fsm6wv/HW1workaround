9c92033eaae81ec8cc08e1f0ab98465b6bff1208
dor.frechter <dor.frechter@gmail.com>
2023-09-04 18:35:00 -0400

    Fixing the nullpointer exception

--- src/main/java/edu/virginia/sde/hw1/JeffersonApportionment.java
+++ src/main/java/edu/virginia/sde/hw1/JeffersonApportionment.java
@@ -3,17 +3,18 @@ package edu.virginia.sde.hw1;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class JeffersonApportionment extends DataReading{
+public class JeffersonApportionment extends DataReading {
 
-    public static int getTotalPopulation(ArrayList<String> list, HashMap<String,Integer> map){
-        int totalpop =0;
-        for( String state: list ){
-            int x = map.get(state);
-            totalpop = totalpop+x;
+    public static int getTotalPopulation(ArrayList<String> list, HashMap<String, Integer> map) {
+        int totalpop = 0;
+        for (String state : list) {
+            if (state != null) {
+                if (map.get(state) != null) {
+                    int x = map.get(state);
+                    totalpop = totalpop + x;
+                }
+            }
         }
         return totalpop;
     }
-    //public static int find
-
-
 }
