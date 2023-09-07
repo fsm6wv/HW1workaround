e15a345fc3cf13b1fed28a4805335a2afdedac8e
dor.frechter <dor.frechter@gmail.com>
2023-09-07 13:38:57 -0400

    split one function into two for easier testing

--- src/main/java/edu/virginia/sde/hw1/JeffersonApportionment.java
+++ src/main/java/edu/virginia/sde/hw1/JeffersonApportionment.java
@@ -17,24 +17,28 @@ public class JeffersonApportionment extends DataReading {
         }
         return totalpop;
     }
-     public static HashMap<String, Integer> findRepNMap(ArrayList<String> statelist, HashMap<String,Integer> datamap, int RealnumOfReps) {
-         int totalpop = getTotalPopulation(statelist, datamap);
-         int divisor = totalpop / RealnumOfReps;
-         int RepCount = 0;
-         while (RepCount != RealnumOfReps) {
-             RepCount = 0;
-             for (String state : statelist) {
-                 RepCount += Math.floor(datamap.get(state) / divisor);
-             }
-             if (RepCount != RealnumOfReps) {
-                 if (RepCount > RealnumOfReps) {
-                     divisor = divisor * 11/10;
-                 }
-                 else{
-                     divisor = divisor*9/10;
-                 }
-             }
-         }
+    public static int findDivisor(ArrayList<String> statelist, HashMap<String,Integer> datamap, int RealnumOfReps){
+        int totalpop = getTotalPopulation(statelist, datamap);
+        int divisor = totalpop / RealnumOfReps;
+        int RepCount = 0;
+        while (RepCount != RealnumOfReps) {
+            RepCount = 0;
+            for (String state : statelist) {
+                RepCount += Math.floor(datamap.get(state) / divisor);
+            }
+            if (RepCount != RealnumOfReps) {
+                if (RepCount > RealnumOfReps) {
+                    divisor = divisor * 11/10;
+                }
+                else{
+                    divisor = divisor*9/10;
+                }
+            }
+        }
+        return divisor;
+    }
+     public static HashMap<String, Integer> makeRepNMap(ArrayList<String> statelist, HashMap<String,Integer> datamap, int RealnumOfReps) {
+         int divisor = findDivisor(statelist,datamap,RealnumOfReps);
          HashMap<String, Integer> congress= new HashMap<>();
          for (String state: statelist){
              congress.put(state, datamap.get(state) / divisor);
